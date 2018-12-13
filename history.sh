5e23e6b352f1de58fc7486ea13fd6cbc9df9f0f2
JorgePlk <jfernandlzf@alumnos.unex.es>
2018-12-13 20:46:43 +0100

    Update history.sh

--- history.sh
+++ history.sh
@@ -7,17 +7,14 @@ apt install apache2;
 service apache2 reload;
 mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf;
 gedit /etc/modsecurity/modsecurity.conf;
-cd /tmp;
-wget -O SpiderLabs-owasp-modsecurity-crs.tar.gz https://github.com/SpiderLabs/owaspmodsecurity-crs/tarball/master;
-tar -zxvf SpiderLabs-owasp-modsecurity-crs.tar.gz;
+wget https://github.com/JorgePlk/Raspberry-Scripts/raw/master/SpiderLabs-owasp-modsecurity-crs-2.2.9-40-g0475e92.tar.gz;
+tar -zxvf SpiderLabs-owasp-modsecurity-crs-2.2.9-40-g0475e92.tar.gz;
 cp -R SpiderLabs-owasp-modsecurity-crs-*/* /etc/modsecurity/;
-rm SpiderLabs-owasp-modsecurity-crs.tar.gz;
+rm SpiderLabs-owasp-modsecurity-crs-2.2.9-40-g0475e92.tar.gz;
 rm -R SpiderLabs-owasp-modsecurity-crs-* ;
 mv /etc/modsecurity/modsecurity_crs_10_setup.conf.example /etc/modsecurity/modsecurity_crs_10_setup.conf;
-cd /etc/modsecurity/base_rules;
-for f in * ; do   ln -s /etc/modsecurity/base_rules/$f /etc/modsecurity/activated_rules/$f ;  done
-cd /etc/modsecurity/optional_rules;
-for f in * ; do   ln -s /etc/modsecurity/optional_rules/$f /etc/modsecurity/activated_rules/$f ;  done
+for f in /etc/modsecurity/base_rules/* ; do   ln -s /etc/modsecurity/base_rules/$f /etc/modsecurity/activated_rules/$f ;  done
+for f in /etc/modsecurity/optional_rules/* ; do   ln -s /etc/modsecurity/optional_rules/$f /etc/modsecurity/activated_rules/$f ;  done
 gedit /etc/modsecurity/modsecurity.conf;
 echo "testear";
 a2enmod headers;
